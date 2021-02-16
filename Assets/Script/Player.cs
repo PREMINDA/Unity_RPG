c6dd533b7d2d050d254c4f8c9eceb9d6173d7e25
PREMINDA <premindabandara0777@gmail.com>
2021-02-17 00:39:36 +0530

    Player Movement

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rg;
+    float speed = 1f;
+    void Start()
+    {
+        _rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float Hmove = Input.GetAxisRaw("Horizontal");
+        float Vmove = Input.GetAxisRaw("Vertical");
+
+
+        _rg.velocity = new Vector2(Hmove *speed, Vmove * speed);
+    }
+}
