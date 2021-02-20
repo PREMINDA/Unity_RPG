1862f335fb8ef3d12391863a2f3da64006f7cb4d
PREMINDA <premindabandara0777@gmail.com>
2021-02-21 00:15:48 +0530

    Fade Screen

---
+++ Assets/Script/FadeUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeUI : MonoBehaviour
+{
+    public Image img;
+    public bool shouldBlack;
+    public bool shouldFade;
+    private float fadeSpeed = 1;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldBlack)
+        {
+            img.color = new Color(img.color.r, img.color.g, img.color.b, Mathf.MoveTowards(img.color.a, 1f, fadeSpeed * Time.deltaTime));
+        }
+    }
+}
