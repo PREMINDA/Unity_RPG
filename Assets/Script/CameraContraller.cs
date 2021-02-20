3c3d492279266f09f8e43d9bc532f77a85caa3b3
PREMINDA <premindabandara0777@gmail.com>
2021-02-20 16:25:02 +0530

    Camera Limit

---
+++ Assets/Script/CameraContraller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraContraller : MonoBehaviour
+{
+    
+    private Transform playerTRG;
+    public Tilemap tileMap;
+
+    private Vector3 bottomLeft;
+    private Vector3 topRight;
+    private float halfHeight;
+    private float halfWidth;
+    
+
+
+    void Start()
+    {
+        playerTRG = Player.instance.transform;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        bottomLeft = tileMap.localBounds.min + new Vector3(halfWidth,halfHeight,0f);
+        topRight = tileMap.localBounds.max + new Vector3(-halfWidth, -halfHeight, 0f);
+        Player.instance.playerLimit(tileMap.localBounds.min, tileMap.localBounds.max);
+         
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTRG.position.x, playerTRG.position.y, transform.position.z);
+
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x,bottomLeft.x,topRight.x), Mathf.Clamp(transform.position.y,bottomLeft.y,topRight.y), transform.position.z);
+        
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rg;
-    float speed = 3f;
+    float speed = 5f;
     private Animator _playerAnimator;
     public static Player instance;
+    public CameraContraller cam;
 
     public string areaTransestionName;
+
+    private Vector3 bottomLeft;
+    private Vector3 topRight;
 
     private void Awake()
     {
@@ -24,12 +28,13 @@ public class Player : MonoBehaviour
     }
     void Start()
     {
-        
+
         _rg = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
         DontDestroyOnLoad(gameObject);
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraContraller>();
+        
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -42,11 +47,20 @@ public class Player : MonoBehaviour
 
 
         _rg.velocity = new Vector2(Hmove *speed, Vmove * speed);
+       
 
-        if(Hmove == 1 || Hmove == -1 || Vmove == 1 || Vmove == -1)
+        if (Hmove == 1 || Hmove == -1 || Vmove == 1 || Vmove == -1)
         {
             _playerAnimator.SetFloat("lastMoveX",Hmove);
             _playerAnimator.SetFloat("lastMoveY", Vmove);
         }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeft.x, topRight.x), Mathf.Clamp(transform.position.y, bottomLeft.y, topRight.y), transform.position.z);
+    }
+
+    public void playerLimit(Vector3 botLeft,Vector3 tpRight)
+    {
+        bottomLeft = botLeft + new Vector3(1f,1f,0);
+        topRight = tpRight + new Vector3(-1f, -1f, 0);
     }
 }
