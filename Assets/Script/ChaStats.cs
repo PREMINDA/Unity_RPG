cb11948ff62d1e815e0791d41eded6661f51d625
PREMINDA <premindabandara0777@gmail.com>
2021-02-27 23:45:22 +0530

    Add Game Manager

--- Assets/Script/ChaStats.cs
+++ Assets/Script/ChaStats.cs
@@ -7,18 +7,21 @@ public class ChaStats : MonoBehaviour
     [SerializeField]
     private string _charName;
     [SerializeField]
-    private int _playLevel;
+    private int _playLevel = 1;
     [SerializeField]
-    private int _currejtExp;
+    private int _currentExp=0;
+    private int _nextLevelExp = 100;
 
     private int _maxHealth = 100;
     public int CurrentHP;
 
-    private int _maxMagicPoint;
+    private int _maxMagicPoint = 50;
     public int _currentHP;
 
-    private int _strength;
-    private int _defence;
+    [SerializeField]
+    private int _strength = 15;
+    [SerializeField]
+    private int _defence = 12;
 
     public int WpPower;
     public int ArPower;
@@ -37,6 +40,39 @@ public class ChaStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            _currentExp += 50;
+            LevelUp();
+        }
         
+    }
+
+
+
+    public void LevelUp()
+    {
+        if (_currentExp >= _nextLevelExp)
+        {
+            _currentExp = 0;
+            _playLevel++;
+            _nextLevelExp += 50;
+            _maxHealth += 2;
+            ArWpPowerup();
+
+
+        }
+    }
+
+    public void ArWpPowerup()
+    {
+        if(_playLevel%2 == 0)
+        {
+            _strength += 5;
+        }
+        else
+        {
+            _defence += 5;
+        }
     }
 }
