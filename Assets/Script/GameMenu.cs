28093f5e333da54feb2b0e2f6534bc2ef2a5153e
PREMINDA <premindabandara0777@gmail.com>
2021-02-28 00:45:26 +0530

    Game Menu Player Stats

---
+++ Assets/Script/GameMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameMenu;
+    void Start()
+    {
+        gameMenu.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameMenu.activeInHierarchy)
+            {
+                gameMenu.SetActive(false);
+            }
+            else
+            {
+                gameMenu.SetActive(true);
+            }
+        }
+    }
+}
